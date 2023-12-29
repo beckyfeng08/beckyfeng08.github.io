6d01bbe865d6784c3414870d9ea7e37ecdf0fb4b
Rebecca Feng <beckyfeng08@berkeley.edu>
2023-12-28 18:32:19 -0800

    scripts

---
+++ scripts/fade.js
@@ -0,0 +1,7 @@
+
+document.getElementById('thenephew').addEventListener('click', function() {
+  const elementToFadeIn = document.getElementById('catpic');
+  
+  elementToFadeIn.classList.remove('hidden');
+  document.getElementById('clickme').classList.remove('hidden');
+});
